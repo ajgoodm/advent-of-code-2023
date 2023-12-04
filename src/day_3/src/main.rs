3ee2442036c8b6874a6bfa8c4d71e8a5475249f1
Aaron Aster <aaronaster@mac-mini.local>
2023-12-03 19:54:49 -0500

    done

--- src/day_3/src/main.rs
+++ src/day_3/src/main.rs
@@ -6,6 +6,8 @@ use shared::input::AocBufReader;
 fn main() {
     let result = part_1(AocBufReader::from_string("inputs/part_1.txt"));
     println!("part 1: {}", result);
+    let result = part_2(AocBufReader::from_string("inputs/part_1.txt"));
+    println!("part 2: {}", result);
 }
 
 fn part_1(reader: AocBufReader) -> usize {
@@ -29,6 +31,45 @@ fn part_1(reader: AocBufReader) -> usize {
                 .is_empty()
         })
         .map(|(number, _)| number)
+        .sum()
+}
+
+fn part_2(reader: AocBufReader) -> usize {
+    let (numbers, symbols) = parse_input(reader);
+    let mut asterisks_and_neighbors: HashMap<UCoord, Vec<usize>> = symbols
+        .into_iter()
+        .filter(|(_, c)| *c == '*')
+        .map(|(coord, _)| (coord, Vec::new()))
+        .collect();
+    let asterisk_coords: HashSet<UCoord> = asterisks_and_neighbors.keys().cloned().collect();
+
+    for (number, number_coords) in numbers {
+        let all_neighbors: HashSet<UCoord> =
+            number_coords
+                .into_iter()
+                .fold(HashSet::new(), |result, coord| {
+                    result
+                        .union(&coord.neighbors())
+                        .cloned()
+                        .collect::<HashSet<UCoord>>()
+                });
+        let neighbor_asterisk_coords: HashSet<UCoord> = all_neighbors
+            .intersection(&asterisk_coords)
+            .cloned()
+            .collect();
+        for asterisk_coord in neighbor_asterisk_coords.iter() {
+            asterisks_and_neighbors
+                .get_mut(asterisk_coord)
+                .unwrap()
+                .push(number);
+        }
+    }
+
+    asterisks_and_neighbors
+        .values()
+        .into_iter()
+        .filter(|numbers| numbers.len() == 2)
+        .map(|numbers| numbers[0] * numbers[1])
         .sum()
 }
 
