0c4b236545cd89846cc3934406e998addb16513e
Aaron Goodman <aarongoodm@gmail.com>
2023-12-28 15:10:10 -0500

    beep boop

--- src/day_24/src/main.rs
+++ src/day_24/src/main.rs
@@ -1,9 +1,11 @@
 use std::collections::{HashMap, HashSet};
 
+use nalgebra::Vector3;
+
 use shared::coords3d::S3Coord;
 use shared::input::AocBufReader;
 
-static VELOCITY_BOUNDS: isize = 100_000;
+static VELOCITY_BOUNDS: isize = 1_000;
 
 fn main() {
     let result = part_1(
@@ -61,146 +63,104 @@ fn part_2(reader: AocBufReader) -> usize {
         }
     }
 
+    let vx = find_velocity_component(velocity_x, R3Component::X);
+    let vy = find_velocity_component(velocity_y, R3Component::Y);
+    let vz = find_velocity_component(velocity_z, R3Component::Z);
+    println!("velocity: ({}, {}, {})", vx, vy, vz);
+
+    // Now that we know the velocity vector, we can find the exact line along
+    // which we must throw the rock. Do this by choosing two skew hail
+    // stone paths and constructing the plane containing the hail stone and our
+    // velocity vector. The rock's actual trajectory must be the intersection
+    // of these two planes.
+    let hail_1 = hail_stones[0].clone();
+    let hail_2 = hail_stones[1].clone();
+    find_hailstone_path(S3Coord::new(vx, vy, vz), hail_1, hail_2);
+
+    0
+}
+
+enum R3Component {
+    X,
+    Y,
+    Z,
+}
+
+fn find_velocity_component(
+    hail_by_velocity_component: HashMap<isize, Vec<Hail>>,
+    r3_component: R3Component,
+) -> isize {
     let mut global_candiates: Vec<HashSet<isize>> = Vec::new();
-    for (vx, hail_stones) in velocity_x
+    for (v, hail_stones) in hail_by_velocity_component
         .into_iter()
         .filter(|(_, hail_stones)| hail_stones.len() > 1)
     {
         let n_hail_stones = hail_stones.len();
-        let mut delta_x: Vec<isize> = Vec::new();
+        let mut delta: Vec<isize> = Vec::new();
         for idx_1 in 0..n_hail_stones {
             for idx_2 in 0..n_hail_stones {
                 if idx_2 > idx_1 {
-                    delta_x.push(isize::abs(
-                        hail_stones[idx_2].position.x - hail_stones[idx_1].position.x,
-                    ));
+                    let d = match r3_component {
+                        R3Component::X => {
+                            hail_stones[idx_2].position.x - hail_stones[idx_1].position.x
+                        }
+                        R3Component::Y => {
+                            hail_stones[idx_2].position.y - hail_stones[idx_1].position.y
+                        }
+                        R3Component::Z => {
+                            hail_stones[idx_2].position.z - hail_stones[idx_1].position.z
+                        }
+                    };
+                    delta.push(isize::abs(d));
                 }
             }
         }
 
-        let mut candidate_vx: HashSet<isize> = HashSet::new();
+        let mut candidate_v: HashSet<isize> = HashSet::new();
         for candidate in -1 * VELOCITY_BOUNDS..VELOCITY_BOUNDS {
-            let diff = isize::abs(candidate - vx);
-            if diff == 0 || delta_x.iter().all(|dy| dy % diff == 0) {
-                candidate_vx.insert(candidate);
+            let diff = isize::abs(candidate - v);
+            if diff == 0 || delta.iter().all(|d_| d_ % diff == 0) {
+                candidate_v.insert(candidate);
             }
         }
 
-        global_candiates.push(candidate_vx);
+        global_candiates.push(candidate_v);
     }
     let first = global_candiates[0].clone();
     let vx: HashSet<isize> = global_candiates.into_iter().fold(first, |acc, next| {
         acc.intersection(&next).cloned().collect::<HashSet<isize>>()
     });
     assert_eq!(vx.len(), 1);
-    let vx = vx.into_iter().next().unwrap();
+    vx.into_iter().next().unwrap()
+}
 
-    let mut global_candiates: Vec<HashSet<isize>> = Vec::new();
-    for (vy, hail_stones) in velocity_y
-        .into_iter()
-        .filter(|(_, hail_stones)| hail_stones.len() > 1)
-    {
-        let n_hail_stones = hail_stones.len();
-        let mut delta_y: Vec<isize> = Vec::new();
-        for idx_1 in 0..n_hail_stones {
-            for idx_2 in 0..n_hail_stones {
-                if idx_2 > idx_1 {
-                    delta_y.push(isize::abs(
-                        hail_stones[idx_2].position.y - hail_stones[idx_1].position.y,
-                    ));
-                }
-            }
-        }
+fn find_hailstone_path(v_rock: S3Coord, h1: Hail, h2: Hail) {
+    // find the plane containing h1's path and the rock's velocity
+    let normal_vec_1 = s3_coord_to_vec_f64(&v_rock).cross(&Vector3::new(h1.vx(), h1.vy(), h1.vz()));
+    let a1 = normal_vec_1.x;
+    let b1 = normal_vec_1.y;
+    let c1 = normal_vec_1.z;
+    let d1 = a1 * h1.rx() + b1 * h1.ry() + c1 * h1.rz();
 
-        let mut candidate_vy: HashSet<isize> = HashSet::new();
-        for candidate in -1 * VELOCITY_BOUNDS..VELOCITY_BOUNDS {
-            let diff = isize::abs(candidate - vy);
-            if diff == 0 || delta_y.iter().all(|dy| dy % diff == 0) {
-                candidate_vy.insert(candidate);
-            }
-        }
+    let normal_vec_2 = s3_coord_to_vec_f64(&v_rock).cross(&Vector3::new(h2.vx(), h2.vy(), h2.vz()));
+    let a2 = normal_vec_2.x;
+    let b2 = normal_vec_2.y;
+    let c2 = normal_vec_2.z;
+    let d2 = a2 * h2.rx() + b2 * h2.ry() + c2 * h2.rz();
 
-        global_candiates.push(candidate_vy);
-    }
-    let first = global_candiates[0].clone();
-    let vy: HashSet<isize> = global_candiates.into_iter().fold(first, |acc, next| {
-        acc.intersection(&next).cloned().collect::<HashSet<isize>>()
-    });
-    assert_eq!(vy.len(), 1);
-    let vy = vy.into_iter().next().unwrap();
+    // at z = 0;
+    let z: f64 = 0.0;
+    let x: f64 = (d2 - (b2 * d1 / b1)) / (a2 - (b2 * a1 / b1));
+    let y: f64 = (d1 - (a1 * x)) / b1;
+    println!("x: {x} - y: {y}");
 
-    let mut global_candiates: Vec<HashSet<isize>> = Vec::new();
-    for (vz, hail_stones) in velocity_z
-        .into_iter()
-        .filter(|(_, hail_stones)| hail_stones.len() > 1)
-    {
-        let n_hail_stones = hail_stones.len();
-        let mut delta_z: Vec<isize> = Vec::new();
-        for idx_1 in 0..n_hail_stones {
-            for idx_2 in 0..n_hail_stones {
-                if idx_2 > idx_1 {
-                    delta_z.push(isize::abs(
-                        hail_stones[idx_2].position.z - hail_stones[idx_1].position.z,
-                    ));
-                }
-            }
-        }
+    let v_rock = s3_coord_to_vec_f64(&v_rock);
+    println!("p0 - {:?}", p0);
+    println!("h1 - {:?}", h1.position);
+}
 
-        let mut candidate_vz: HashSet<isize> = HashSet::new();
-        for candidate in -1 * VELOCITY_BOUNDS..VELOCITY_BOUNDS {
-            let diff = isize::abs(candidate - vz);
-            if diff == 0 || delta_z.iter().all(|dz| dz % diff == 0) {
-                candidate_vz.insert(candidate);
-            }
-        }
-
-        global_candiates.push(candidate_vz);
-    }
-    let first = global_candiates[0].clone();
-    let vz: HashSet<isize> = global_candiates.into_iter().fold(first, |acc, next| {
-        acc.intersection(&next).cloned().collect::<HashSet<isize>>()
-    });
-    assert_eq!(vz.len(), 1);
-    let vz = vz.into_iter().next().unwrap();
-
-    println!("velocity: ({}, {}, {})", vx, vy, vz);
-
-    let hail_1 = hail_stones[0].clone();
-    let hail_2 = hail_stones[1].clone();
-
-    let mut candidates_h1: HashSet<S3Coord> = HashSet::new();
-    let mut candidates_h2: HashSet<S3Coord> = HashSet::new();
-    for t in 0..100_000_000isize {
-        let h1_position = S3Coord::new(
-            hail_1.position.x + (t * hail_1.velocity.x),
-            hail_1.position.y + (t * hail_1.velocity.y),
-            hail_1.position.z + (t * hail_1.velocity.z),
-        );
-        candidates_h1.insert(S3Coord::new(
-            h1_position.x - (t * vx),
-            h1_position.y - (t * vy),
-            h1_position.z - (t * vz),
-        ));
-
-        let h2_position = S3Coord::new(
-            hail_2.position.x + (t * hail_2.velocity.x),
-            hail_2.position.y + (t * hail_2.velocity.y),
-            hail_2.position.z + (t * hail_2.velocity.z),
-        );
-        candidates_h2.insert(S3Coord::new(
-            h2_position.x - (t * vx),
-            h2_position.y - (t * vy),
-            h2_position.z - (t * vz),
-        ));
-    }
-
-    let final_positions: HashSet<S3Coord> = candidates_h1
-        .intersection(&candidates_h2)
-        .cloned()
-        .collect();
-    println!("{:?}", final_positions);
-
-    0
+fn s3_coord_to_vec_f64(u: &S3Coord) -> Vector3<f64> {
+    Vector3::new(u.x as f64, u.y as f64, u.z as f64)
 }
 
 #[derive(Clone)]
