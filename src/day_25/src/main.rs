952e0ab003d59f4601432b94862200f11f024d1f
Aaron Aster <aaronaster@mac-mini.local>
2023-12-28 20:34:45 -0500

    developing along

---
+++ src/day_25/src/main.rs
@@ -0,0 +1,10 @@
+use shared::input::AocBufReader;
+
+fn main() {
+    let result = part_1(AocBufReader::from_string("inputs/test.txt"));
+    println!("part 1: {result}");
+}
+
+fn part_1(reader: AocBufReader) -> usize {
+    0
+}
